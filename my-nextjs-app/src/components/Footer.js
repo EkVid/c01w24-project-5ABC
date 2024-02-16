cb01aac10eddae0b13c0e51dcc2224e7b8e5c819
Austin <austinjas014@gmail.com>
2024-02-16 11:55:57 -0500

    Created footer section

---
+++ my-nextjs-app/src/components/Footer.js
@@ -0,0 +1,48 @@
+const Footer = () => {
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className="bg-[#263238] text-white">
+      <div className="max-w-6xl mx-auto px-4 py-10 md:py-12 flex flex-col justify-between h-full">
+        {/* Logo and Navigation Container */}
+        <div className="flex flex-col items-center justify-center">
+          {/* Logo */}
+          <div>
+            <h2 className="uppercase font-bold">Logo</h2>
+          </div>
+          {/* Navigation/Quick Links */}
+          <ul className="flex mt-5 justify-center lg:space-x-40 md:space-x-20 space-x-5">
+            <li>
+              <a href="#" className="hover:underline">
+                Dashboard
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline">
+                Grants
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline">
+                Application
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline">
+                Edit&nbsp;Profile
+              </a>
+            </li>
+          </ul>
+        </div>
+        {/* Copyright Section */}
+        <div className="mt-8 flex justify-center w-full">
+          <p className="text-center">
+            Copyright &copy; {year} 5ABC. All Rights Reserved
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
