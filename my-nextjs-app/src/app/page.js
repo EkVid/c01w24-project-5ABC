41a2e54d507768f28524f8734bd83ad491525822
auricz <auric.zhou@gmail.com>
2024-02-17 14:18:17 -0500

    added quotes section to landing page

--- my-nextjs-app/src/app/page.js
+++ my-nextjs-app/src/app/page.js
@@ -1,9 +1,11 @@
 import Hero from "@/components/Hero";
+import Quotes from "@/components/Quotes";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Quotes />
     </main>
   );
 }
