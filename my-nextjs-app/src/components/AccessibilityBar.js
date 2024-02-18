3c39dd2d6bfb51e6c118586a4064a0b0daa5d390
CarterMorris <carter.morris@mail.utoronto.ca>
2024-02-17 19:49:50 -0500

    css fixes for small screens

--- my-nextjs-app/src/components/AccessibilityBar.js
+++ my-nextjs-app/src/components/AccessibilityBar.js
@@ -1,6 +1,8 @@
 'use client'
 
 import "@/app/globals.css";
+import chevronDown from "../../public/chevron-down.svg"
+import Image from "next/image.js";
 import { useState } from "react";
 import { scaleFont, resetFont } from "./utils/scaleFont.js"
 import { initTheme, changeTheme, getTheme } from "./utils/theme.js"
@@ -12,7 +14,7 @@ const AccessibilityBar = () => {
   return(
     <div className="custom-dark-grey-background dark:bg-[#263238] drop-shadow-sm">
       {/* Dropdown */}
-      <details className="group ">
+      <details className="group">
 
         {/* Dropdown Closed Content */}
         <summary className="list-none flex flex-nowrap justify-end items-center cursor-pointer">
@@ -23,10 +25,10 @@ const AccessibilityBar = () => {
         </summary>
 
         {/* Dropdown Open Content */}
-        <div className="flex custom-dark-grey-text p-4 space-x-5 transition-all">
+        <div className="flex justify-start sm:justify-center custom-dark-grey-text p-4 space-x-5 transition-all overflow-x-auto">
 
           {/* Font Size */}
-          <div className="flex flex-col p-2 rounded-lg drop-shadow-lg custom-offwhite-background dark:bg-[#1f1f1f] border-2 border-transparent dark:border-gray-600">
+          <div className="flex flex-col p-2 rounded-lg drop-shadow-lg custom-offwhite-background dark:bg-[#1f1f1f] border-2 border-transparent dark:border-gray-600  min-w-40">
             <div className="flex items-center justify-center">
               <h3 className="text-center cs-text-xl dark:text-white">Font Size </h3>
               <button className="cs-text-2xl ms-3 cursor-pointer dark:text-white" onClick={resetFont}>&#8635;</button>
@@ -51,30 +53,43 @@ const AccessibilityBar = () => {
           </div>
 
           {/* Theme */}
-          <div className="flex flex-col p-2 rounded-lg drop-shadow-lg custom-offwhite-background dark:bg-[#1f1f1f] border-2 border-transparent dark:border-gray-600">
+          <div className="flex flex-col p-2 rounded-lg drop-shadow-lg custom-offwhite-background dark:bg-[#1f1f1f] border-2 border-transparent dark:border-gray-600 min-w-40">
             <h3 className="text-center cs-text-xl dark:text-white">Theme</h3>
-            <div className="flex space-x-2 items-center mt-4">
-              <p className="cs-text-lg dark:text-white">Dark</p>
+            <div className="flex flex-col lg:flex-row lg:space-x-2 items-center lg:justify-center mt-4">
+              <p className="cs-text-lg dark:text-white -ms-12 lg:ms-0">Dark</p>
               <label htmlFor="theme" className="relative cursor-pointer focus-visible:ring focus-visible:ring-[#E0E0E0] ">
                 <input id='theme' type="checkbox" className="sr-only peer" onChange={(e) => changeTheme(e.target.checked, setLightTheme)} checked={lightTheme}/>
-                <div className="dark:bg-[#000000] bg-[#4CAF4F] relative w-20 h-10 rounded-full transition-colors duration-400 z-0"></div>
+                <div className="dark:bg-[#E0E0E0] bg-[#4CAF4F] relative w-20 h-10 rounded-full transition-colors duration-400 z-0"></div>
                 <span className="w-2/5 h-4/5 bg-white absolute rounded-full left-1 top-1 peer-checked:left-11 transition-all duration-400 z-1"></span>
               </label>
-              <p className="cs-text-lg dark:text-white">Light</p>
+              <p className="cs-text-lg dark:text-white ms-12 lg:ms-0">Light</p>
             </div>
           </div>
 
           {/* Language */}
-          <div className="flex flex-col p-2 rounded-lg drop-shadow-lg custom-offwhite-background dark:bg-[#1f1f1f] border-2 border-transparent dark:border-gray-600">
+          <div className="flex flex-col p-2 rounded-lg drop-shadow-lg custom-offwhite-background dark:bg-[#1f1f1f] border-2 border-transparent dark:border-gray-600 min-w-40">
             <h3 className="text-center cs-text-xl dark:text-white">Language</h3>
+            <div className="flex justify-center items-center mt-4 mx-1 px-2 rounded-lg custom-dark-grey-background h-8">
+              <p className="cs-text-xl text-center ms-auto">EN</p>
+              <Image
+                src={chevronDown}
+                alt="down chevron"
+                className="ms-auto transition-all duration-300"
+              />
+            </div>
+          </div>
+
+          {/* Reduced Motion */}
+          <div className="flex flex-col p-2 rounded-lg drop-shadow-lg custom-offwhite-background dark:bg-[#1f1f1f] border-2 border-transparent dark:border-gray-600 min-w-40">
+            <h3 className="text-center cs-text-xl dark:text-white">Reduced Motion</h3>
             <div className="flex space-x-2 items-center mt-4">
-              <p className="cs-text-lg dark:text-white">Dark</p>
-              <label htmlFor="theme" className="relative cursor-pointer focus-visible:ring focus-visible:ring-[#E0E0E0] ">
-                <input id='theme' type="checkbox" className="sr-only peer" onChange={(e) => changeTheme(e.target.checked, setLightTheme)} checked={lightTheme}/>
-                <div className="dark:bg-[#000000] bg-[#4CAF4F] relative w-20 h-10 rounded-full transition-colors duration-400 z-0"></div>
+              <p className="cs-text-lg dark:text-white">Off</p>
+              <label htmlFor="motion" className="relative cursor-pointer focus-visible:ring focus-visible:ring-[#E0E0E0] ">
+                <input id='motion' type="checkbox" className="sr-only peer"/>
+                <div className="bg-[#E0E0E0] peer-checked:bg-[#4CAF4F] relative w-20 h-10 rounded-full transition-colors duration-400 z-0"></div>
                 <span className="w-2/5 h-4/5 bg-white absolute rounded-full left-1 top-1 peer-checked:left-11 transition-all duration-400 z-1"></span>
               </label>
-              <p className="cs-text-lg dark:text-white">Light</p>
+              <p className="cs-text-lg dark:text-white">On</p>
             </div>
           </div>
         </div>
