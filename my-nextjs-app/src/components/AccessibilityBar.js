ca188fc56c4f5c59dab6aafcfa5f5b450b85746d
CarterMorris <carter.morris@mail.utoronto.ca>
2024-02-17 19:19:53 -0500

    Font Size and Theme settings implemented

--- my-nextjs-app/src/components/AccessibilityBar.js
+++ my-nextjs-app/src/components/AccessibilityBar.js
@@ -1,6 +1,7 @@
 'use client'
 
 import "@/app/globals.css";
+<<<<<<< HEAD
 import chevronDown from "../../public/chevron-down.svg"
 import sun from "../../public/sun.svg"
 import moon from "../../public/moon.svg"
@@ -51,6 +52,20 @@ const AccessibilityBar = ({children, onChangeTheme, onChangeFont, onChangeMotion
     <div className="h-fit custom-dark-grey-background dark:bg-[#263238] drop-shadow-sm">
       {/* Dropdown */}
       <details className="group">
+=======
+import { useState } from "react";
+import { scaleFont, resetFont } from "./utils/scaleFont.js"
+import { initTheme, changeTheme, getTheme } from "./utils/theme.js"
+
+const AccessibilityBar = () => {
+  initTheme()
+  const [ lightTheme, setLightTheme ] = useState(getTheme() === 'light')
+
+  return(
+    <div className="custom-dark-grey-background dark:bg-[#263238] drop-shadow-sm">
+      {/* Dropdown */}
+      <details className="group ">
+>>>>>>> fae0b24 (Font Size and Theme settings implemented)
 
         {/* Dropdown Closed Content */}
         <summary className="list-none flex flex-nowrap justify-end items-center cursor-pointer">
@@ -61,6 +76,7 @@ const AccessibilityBar = ({children, onChangeTheme, onChangeFont, onChangeMotion
         </summary>
 
         {/* Dropdown Open Content */}
+<<<<<<< HEAD
         <div className="flex justify-start custom-dark-grey-text p-4 space-x-5 transition-all overflow-x-auto">
 
           {/* Font Size */}
@@ -68,12 +84,25 @@ const AccessibilityBar = ({children, onChangeTheme, onChangeFont, onChangeMotion
             <div className="flex items-center justify-center">
               <h3 className="text-center cs-text-xl dark:text-white">Font Size </h3>
               <button className="cs-text-2xl ms-3 cursor-pointer dark:text-white" onClick={handleResetFont}>&#8635;</button>
+=======
+        <div className="flex custom-dark-grey-text p-4 space-x-5 transition-all">
+
+          {/* Font Size */}
+          <div className="flex flex-col p-2 rounded-lg drop-shadow-lg custom-offwhite-background dark:bg-[#1f1f1f] border-2 border-transparent dark:border-gray-600">
+            <div className="flex items-center justify-center">
+              <h3 className="text-center cs-text-xl dark:text-white">Font Size </h3>
+              <button className="cs-text-2xl ms-3 cursor-pointer dark:text-white" onClick={resetFont}>&#8635;</button>
+>>>>>>> fae0b24 (Font Size and Theme settings implemented)
             </div>
             
             <div className="flex space-x-4 justify-center items-center">
               <button 
                 className="cs-text-5xl font-light hover:cursor-pointer dark:text-white" 
+<<<<<<< HEAD
                 onClick={handleScaleFontDown}>
+=======
+                onClick={() => scaleFont('down')}>
+>>>>>>> fae0b24 (Font Size and Theme settings implemented)
                   −
               </button>
 
@@ -81,7 +110,11 @@ const AccessibilityBar = ({children, onChangeTheme, onChangeFont, onChangeMotion
 
               <button 
                 className="cs-text-5xl font-light hover:cursor-pointer dark:text-white" 
+<<<<<<< HEAD
                 onClick={handleScaleFontUp}>
+=======
+                onClick={() => scaleFont('up')}>
+>>>>>>> fae0b24 (Font Size and Theme settings implemented)
                   +
               </button>
             </div>
@@ -89,6 +122,7 @@ const AccessibilityBar = ({children, onChangeTheme, onChangeFont, onChangeMotion
           </div>
 
           {/* Theme */}
+<<<<<<< HEAD
           <div className="flex flex-col p-2 rounded-lg drop-shadow-lg custom-offwhite-background dark:bg-[#1f1f1f] border-2 border-transparent dark:border-gray-600 min-w-40">
             <h3 className="text-center cs-text-xl dark:text-white">Theme</h3>
             <div className="flex flex-col lg:flex-row lg:space-x-2 items-center lg:justify-center mt-4">
@@ -103,10 +137,23 @@ const AccessibilityBar = ({children, onChangeTheme, onChangeFont, onChangeMotion
                   />
                 </span>
               </label>
+=======
+          <div className="flex flex-col p-2 rounded-lg drop-shadow-lg custom-offwhite-background dark:bg-[#1f1f1f] border-2 border-transparent dark:border-gray-600">
+            <h3 className="text-center cs-text-xl dark:text-white">Theme</h3>
+            <div className="flex space-x-2 items-center mt-4">
+              <p className="cs-text-lg dark:text-white">Dark</p>
+              <label htmlFor="theme" className="relative cursor-pointer focus-visible:ring focus-visible:ring-[#E0E0E0] ">
+                <input id='theme' type="checkbox" className="sr-only peer" onChange={(e) => changeTheme(e.target.checked, setLightTheme)} checked={lightTheme}/>
+                <div className="dark:bg-[#000000] bg-[#4CAF4F] relative w-20 h-10 rounded-full transition-colors duration-400 z-0"></div>
+                <span className="w-2/5 h-4/5 bg-white absolute rounded-full left-1 top-1 peer-checked:left-11 transition-all duration-400 z-1"></span>
+              </label>
+              <p className="cs-text-lg dark:text-white">Light</p>
+>>>>>>> fae0b24 (Font Size and Theme settings implemented)
             </div>
           </div>
 
           {/* Language */}
+<<<<<<< HEAD
           <div className="flex flex-col p-2 rounded-lg drop-shadow-lg custom-offwhite-background dark:bg-[#1f1f1f] border-2 border-transparent dark:border-gray-600 min-w-40">
             <h3 className="text-center cs-text-xl dark:text-white">Language</h3>
             <div className="flex justify-center items-center mt-4 mx-1  rounded-lg custom-dark-grey-background h-8">
@@ -130,10 +177,23 @@ const AccessibilityBar = ({children, onChangeTheme, onChangeFont, onChangeMotion
                 <span className="w-2/5 h-4/5 bg-white absolute rounded-full left-1 top-1 peer-checked:left-11 transition-all duration-400 z-1"></span>
               </label>
               <p className="cs-text-lg dark:text-white">On</p>
+=======
+          <div className="flex flex-col p-2 rounded-lg drop-shadow-lg custom-offwhite-background dark:bg-[#1f1f1f] border-2 border-transparent dark:border-gray-600">
+            <h3 className="text-center cs-text-xl dark:text-white">Language</h3>
+            <div className="flex space-x-2 items-center mt-4">
+              <p className="cs-text-lg dark:text-white">Dark</p>
+              <label htmlFor="theme" className="relative cursor-pointer focus-visible:ring focus-visible:ring-[#E0E0E0] ">
+                <input id='theme' type="checkbox" className="sr-only peer" onChange={(e) => changeTheme(e.target.checked, setLightTheme)} checked={lightTheme}/>
+                <div className="dark:bg-[#000000] bg-[#4CAF4F] relative w-20 h-10 rounded-full transition-colors duration-400 z-0"></div>
+                <span className="w-2/5 h-4/5 bg-white absolute rounded-full left-1 top-1 peer-checked:left-11 transition-all duration-400 z-1"></span>
+              </label>
+              <p className="cs-text-lg dark:text-white">Light</p>
+>>>>>>> fae0b24 (Font Size and Theme settings implemented)
             </div>
           </div>
         </div>
       </details>
+<<<<<<< HEAD
       <FontSizeContext.Provider value={fontSize}>
         <ThemeContext.Provider value={lightTheme}>
           <ReducedMotionContext.Provider value={isReducedMotion}>
@@ -141,6 +201,8 @@ const AccessibilityBar = ({children, onChangeTheme, onChangeFont, onChangeMotion
           </ReducedMotionContext.Provider>
         </ThemeContext.Provider>
       </FontSizeContext.Provider>
+=======
+>>>>>>> fae0b24 (Font Size and Theme settings implemented)
     </div>
   )
 }
--- my-nextjs-app/src/components/Hero.js
+++ my-nextjs-app/src/components/Hero.js
@@ -24,6 +24,7 @@ const Hero = () => {
     return () => clearInterval(interval); // Cleanup interval on component unmount
   }, [fontSize]);
   return (
+<<<<<<< HEAD
     <div className="grid grid-cols-1 md:grid-cols-2 gap-20 py-16 md:px-8 lg:px-12 items-center justify-center md:ml-20 sm:ml-10 min-w-">
       <div className="flex justify-end items-center">
         <div className="flex flex-col gap-8 items-center text-center lg:items-start lg:text-left text-align:ceter">
@@ -40,6 +41,23 @@ const Hero = () => {
             Register
           </button>
         </div>
+=======
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-20 py-16 px-4 md:px-8 lg:px-12 items-center lg:ml-40 md:ml-20">
+      {" "}
+      <div className="flex flex-col gap-8 lg:ml-20">
+        <h1 className="text-black dark:text-white text-3xl tracking-widest md:text-4xl lg:text-5xl font-semibold">
+          Funding futures, <span className="custom-green"> changing lives</span>
+          .
+        </h1>
+        <p className="text-black text-sm md:text-sm lg:text-sm custom-grey dark:text-[#dddddd]">
+          Simplify your life with MA everywhere, our online and mobile
+          self-service platform that lets you instantly connect to your grants
+          information.
+        </p>
+        <button className="self-start bg-blue-500 text-white py-2 px-4 rounded custom-green-background">
+          Register
+        </button>
+>>>>>>> fae0b24 (Font Size and Theme settings implemented)
       </div>
       <div className="flex justify-center">
         <Image
