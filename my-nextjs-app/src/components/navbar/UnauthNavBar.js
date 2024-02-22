ac36179c0efd5a48ffda1e206cb1c0ff7025419a
Austin <austinjas014@gmail.com>
2024-02-22 08:57:46 -0500

    Organized stylesheet and fixed the css

--- my-nextjs-app/src/components/navbar/UnauthNavBar.js
+++ my-nextjs-app/src/components/navbar/UnauthNavBar.js
@@ -1,6 +1,5 @@
-
-import Link from 'next/link';
-import styles from '../styles/unauthNavBar.module.css';
+import Link from "next/link";
+import styles from "@/styles/Navbar/unauthNavBar.module.css";
 
 const NewBar = () => {
   return (
