4bbf97e6f871d60562e79295ef8d1a8f1aa6c7c6
Austin <austinjas014@gmail.com>
2024-02-22 18:22:27 -0500

    Created sign up and sign in sections' UI

--- my-nextjs-app/src/app/(root)/page.js
+++ my-nextjs-app/src/app/(root)/page.js
@@ -3,6 +3,12 @@ import Hero from "@/components/Hero";
 import Quotes from "@/components/Quotes/Quotes";
 import AuthNavBar from "@/components/navbar/AuthNavBar";
 import UnauthNavBar from "@/components/navbar/UnauthNavBar";
+<<<<<<< HEAD:my-nextjs-app/src/app/(root)/page.js
+=======
+import Login from "@/components/Login";
+import SignUp from "@/components/Signup";
+
+>>>>>>> c0cda00 (Created sign up and sign in sections' UI):my-nextjs-app/src/app/page.js
 export default function Home() {
 
   return (
@@ -10,7 +16,9 @@ export default function Home() {
       <AuthNavBar />
       <AddFormButton />
       <Hero />
-      <Quotes />
+      <Quotes /> */}
+      {/* <Login /> */}
+      <SignUp />
     </main>
   );
 }
