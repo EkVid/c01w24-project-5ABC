29b028b579aefb5f218b05319680343bc63a6a28
samnaniaahil <aahil.samnani@yahoo.com>
2024-02-25 15:45:49 -0500

    Fix data model formatting

--- backend/dataModels.py
+++ backend/dataModels.py
@@ -1,51 +1,23 @@
 from pydantic import BaseModel, validator, PositiveInt
 from annotated_types import Len
 from typing import Literal, Optional, Annotated, Union
-from datetime import date
-from enum import IntEnum
-
 
 """
-Constants
+Grant Model
 """
-class ApplicationStatus(IntEnum):
-    DRAFT = 0
-    IN_REVIEW = 1
-    REJECTED = 2
-    APPROVED = 3
-
-# TODO: put constants here for veteran status once decided
-class VeteranStatus(IntEnum):
-    VETERAN = 0
-    NON_VETERAN = 1  # in what case would we want to exclude a veteran from applying?
-
-"""
-User Profile Models
-"""
-class UserProfileReqs(BaseModel):
-    # Optional still means you have to pass in these fields but just with None or "", etc
-    minAge: Optional[int]
-    maxAge: Optional[int]
-    race: Optional[list[str]]
-    gender: Optional[list[str]]
-    nationality: Optional[list[str]]
-    veteran: Optional[int]
-
-class UserProfile(BaseModel):
-    age: int
-    race: str
-    gender: str
-    nationality: str
-    veteran: int
-
+class Grant(BaseModel):
+    grantorID: int
+    grantName: str
+    quantity: PositiveInt
 
 """
 Question Option and Question Models
 """
 class TextboxOptions(BaseModel):
+    answerType: str
     minCharsNum: PositiveInt
     maxCharsNum: PositiveInt
-    isMultipleLines: Optional[bool] = None
+    isMultipleLines: bool
 
 class NumberOptions(BaseModel):
     isIntegerOnly: bool
@@ -53,15 +25,13 @@ class NumberOptions(BaseModel):
     maxNum: Union[int, float]
 
     @validator('minNum', 'maxNum', always=True)
-    def validate_range(cls, value, values) -> Union[int, float]:
+    def validate_range(cls, value, values):
         if values['isIntegerOnly']:
             assert(type(value) == int)
         else:
             assert(type(value) == float)
 
-        return value
-
-class MultipleChoiceOptions(BaseModel):
+class MultipleChoiceOptions(BaseModel): 
     answers: Annotated[list[str], Len(min_length=2, max_length=10)]
 
 class CheckboxOptions(BaseModel):
@@ -69,119 +39,30 @@ class CheckboxOptions(BaseModel):
     isNoneAnOption: bool
 
 class DateOptions(BaseModel):
-    isDateRange: Optional[bool] = None
-    isBothRequired: Optional[bool] = None
+    isDateRange: bool
+    isBothRequired: bool
 
 class FileOptions(BaseModel):
     type: str
 
 class Question(BaseModel):
     question: str
-    type: Literal['textbox', 'number', 'multiple choice', 'checkbox', 'date', 'file', 'email', 'phone number']
+    type: Literal['textbox', 'number', 'multiple choice', 'checkbox', 'date', 'file']
     isRequired: bool
-    options: Optional[Union[
+    options: Union[
         TextboxOptions,
         NumberOptions,
         MultipleChoiceOptions,
         CheckboxOptions,
         DateOptions,
         FileOptions
-    ]] = None
-
-
-"""
-Answer Models
-"""
-class TextboxAnswer(BaseModel):
-    options: TextboxOptions
-    text: str
-
-    @validator('text', always=True)
-    def validate_textbox(cls, value, values):
-        # TODO: check that "options" is a valid key and minCharsNum/maxCharsNum are present
-        # This would cause an error when attempting to update a non-existent application
-        minChars = values['options'].minCharsNum
-        maxChars = values['options'].maxCharsNum
-        inRange = minChars <= len(value) and len(value) <= maxChars
-        assert(inRange == True)
-
-        return value
-
-class NumberAnswer(BaseModel):
-    options: NumberOptions
-    num: Union[int, float]
-
-    @validator('num', always=True)
-    def validate_number(cls, value, values):
-        minNum = values['options'].minNum
-        maxNum = values['options'].maxNum
-        validNum = ((type(value) == type(minNum)) and (minNum <= value <= maxNum))
-        assert(validNum == True)
-
-        return value
-
-class MultipleChoiceAnswer(BaseModel):
-    options: MultipleChoiceOptions
-    selectedChoice: Annotated[list[str], Len(min_length=1, max_length=1)]
-
-class CheckboxAnswer(BaseModel):
-    options: CheckboxOptions
-    selectedBoxes: list[str]
-
-    @validator('selectedBoxes', always=True)
-    def validate_checkbox(cls, value, values):
-        if len(value) == 0:
-            validAnswer = values['options'].isNoneAnOption
-        else:
-            validAnswer = True
-        assert(validAnswer == True)
-
-        return value
-
-class DateAnswer(BaseModel):
-    options: DateOptions
-    date: date
-
-class FileAnswer(BaseModel):
-    options: FileOptions
-    type: str
-
+    ]
 
 """
-Grant Model
+Form Model
 """
-class GrantBase(BaseModel):
-    grantorEmail: str
-    Title: str
-    Description: str
-    NumWinners: int
-    MaxWinners: int
-    Deadline: date # maybe change to str for simplicity
-    PostedDate: date
-    Active: bool
-    AmountPerApp: float
-    profileReqs: UserProfileReqs # test later
-
-class Grant(GrantBase):
-   WinnerIDs: list[str]
-   AppliedIDs: list[str]
-   QuestionData: list[Question]
-
-
-"""
-Application Model
-"""
-class Application(BaseModel):
-    grantID: str
-    email: str
-    dateSubmitted: date
-    status: int
-    profileData: UserProfile
-    answerData: list[Union[
-        TextboxAnswer,
-        NumberAnswer,
-        MultipleChoiceAnswer,
-        CheckboxAnswer,
-        DateAnswer,
-        FileAnswer
-    ]]
+class Form(BaseModel):
+    grantID: int
+    grantorID: int
+    grantName: str
+    questionData: list[Question]
