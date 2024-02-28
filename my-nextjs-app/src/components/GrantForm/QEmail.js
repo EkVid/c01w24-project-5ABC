8a3494b7d3231a0f0594db306bfc6579aab4a7e9
auricz <auric.zhou@gmail.com>
2024-02-28 15:28:55 -0500

    added support for email questions and more styling changes

---
+++ my-nextjs-app/src/components/GrantForm/QEmail.js
@@ -0,0 +1,35 @@
+import { useContext, useState } from "react";
+import ReducedMotionContext from "../utils/ReducedMotionContext";
+import OptionsDiv from "./SmallComponents/OptionsDiv";
+
+const QEmail = ({isErr, isEditMode, onSelectAnswer, onChangeOptions}) => {
+  const [currentAnswer, setCurrentAnswer] = useState("");
+  const isReduceMotion = useContext(ReducedMotionContext);
+
+  const handleOnInput = (newAnswer) => {
+    if (isEditMode) return;
+    setCurrentAnswer(newAnswer);
+    onSelectAnswer(newAnswer);
+  }
+
+  return (
+    <>
+      {/* Standardize the margin between required question option and input */}
+      {isEditMode ? 
+        <OptionsDiv/>
+        :
+        <></>
+      }
+      <input
+        type="email"
+        placeholder={"example@domain.com"}
+        className={`text-sm max-w-full custom-text dark:d-text md:max-w-96 border-b-2 bg-transparent ${isEditMode ? "custom-disabled-input dark:d-custom-disabled-input" : "custom-interactive-input"} ${isErr && !isEditMode ? "custom-err-border" : "dark:border-white "} ${isReduceMotion ? "" : "transition-colors"}`}
+        onInput={e => handleOnInput(e.target.value)}
+        value={currentAnswer}
+        disabled={isEditMode}
+      />
+    </>
+  )
+}
+
+export default QEmail;
