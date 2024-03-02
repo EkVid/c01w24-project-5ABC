48631de62a7373da98571778b3fb0741aeba3d92
auricz <auric.zhou@gmail.com>
2024-03-01 23:28:46 -0500

    moved routers and removed warnings in inspect console

--- my-nextjs-app/src/app/formEditor/layout.js
+++ my-nextjs-app/src/app/formEditor/layout.js
@@ -1,3 +1,7 @@
 export default function FormLayout({children}) {
-  return children;
+  return (
+    <div className="flex flex-col min-h-screen flex-grow dark:bg-[#1f1f1f]">
+      {children}
+    </div>
+  )
 }
