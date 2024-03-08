6ea51b97ef8930e46f2818b3177dbbfe3c327884
alykhan02 <aversi07@gmail.com>
2024-03-08 12:05:26 -0500

    rename navbar

--- my-nextjs-app/src/app/(root)/page.js
+++ my-nextjs-app/src/app/(root)/page.js
@@ -1,13 +1,13 @@
 import AddFormButton from "@/components/GrantForm/AddFormButton";
 import Hero from "@/components/Hero";
 import Quotes from "@/components/Quotes/Quotes";
-// import AuthNavBar from "@/components/navbar/AuthNavBar";
+import AuthNavBar from "@/components/navbar/AuthNavBar";
 import UnauthNavBar from "@/components/navbar/UnauthNavBar";
 export default function Home() {
 
   return (
     <main>
-      <UnauthNavBar />
+      <AuthNavBar />
       <AddFormButton />
       <Hero />
       <Quotes />
