2ef8a570ee24eb21f1771c10ded85163b69d7477
Austin <austinjas014@gmail.com>
2024-03-08 18:10:51 -0500

    Added detail view for dashboard

--- my-nextjs-app/src/components/Grantee_Board/Grantee_dashboard.js
+++ my-nextjs-app/src/components/Grantee_Board/Grantee_dashboard.js
@@ -3,11 +3,12 @@
 import dynamic from "next/dynamic";
 import React from "react";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const Grantee_dashboard = () => {
   const name = "Rawad";
   // Mock data for claims
-  const recentClaims = [
+  const recentGrants = [
     {
       id: 1,
       title: "Medical Expense Claim",
@@ -37,6 +38,8 @@ const Grantee_dashboard = () => {
     },
   ];
 
+  const [expandedGrantId, setExpandedGrantId] = useState(null);
+
   // Function to get the color based on the claim status
   const getStatusColor = (status) => {
     switch (status) {
@@ -48,6 +51,15 @@ const Grantee_dashboard = () => {
         return "bg-yellow-600"; // Yellow for pending
       default:
         return "bg-gray-400"; // Gray for any other status or if no status is provided
+    }
+  };
+
+  const toggleCardExpansion = (id) => {
+    if (expandedGrantId === id) {
+      // If already expanded, shrink the card
+      setExpandedGrantId(null);
+    } else {
+      setExpandedGrantId(id);
     }
   };
 
@@ -71,7 +83,7 @@ const Grantee_dashboard = () => {
                 Hello, {name}!
               </h1>
               <div className="flex flex-col sm:flex-row justify-between items-center space-y-4 sm:space-y-0 mb-4">
-                <h2 className="text-xl font-semibold text-black">
+                <h2 className="text-xl font-semibold text-black text-center sm:text-left">
                   Your Recent Applications
                 </h2>
 
@@ -90,36 +102,217 @@ const Grantee_dashboard = () => {
                 </div>
               </div>
 
-              {recentClaims.length > 0 ? (
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 text-black">
-                  {recentClaims.map((claim) => (
-                    <div key={claim.id} className="p-2">
-                      <div
-                        className={`${getStatusColor(
-                          claim.status
-                        )} h-2 w-full rounded-t-lg`}
-                      ></div>
-                      {/* Card content */}
-                      <div className="bg-white p-4 rounded-lg shadow shadow-xl">
-                        <div className="flex justify-between items-center mb-4">
-                          <p className="text-md">Status: {claim.status}</p>
-                          <button className="text-green-700 text-md hover:text-green-900 transition duration-150 ease-in-out">
-                            View Details
-                          </button>
+              {recentGrants.length > 0 ? (
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 text-black mt-3">
+                  {recentGrants.map((claim) => {
+                    // Check if the card should be expanded or if no card is expanded
+                    const shouldDisplay =
+                      expandedGrantId === null || expandedGrantId === claim.id;
+
+                    // Only render the card if it should be displayed based on the above condition
+                    if (shouldDisplay) {
+                      return (
+                        <div
+                          key={claim.id}
+                          className={` p-1 transition-all duration-500 ease-in-out ${
+                            expandedGrantId === claim.id
+                              ? "scale-100 opacity-100"
+                              : "scale-95 opacity-75"
+                          } ${
+                            expandedGrantId === claim.id
+                              ? "col-span-3 lg:col-span-3"
+                              : "md:col-span-2 lg:col-span-1"
+                          }`}
+                        >
+                          <div
+                            className={`${getStatusColor(
+                              claim.status
+                            )} h-2 w-full rounded-t-lg`}
+                          ></div>
+
+                          <div className="bg-white p-2 rounded-lg shadow shadow-xl">
+                            <div className="flex justify-between items-center mb-4">
+                              <p className="text-md">Status: {claim.status}</p>
+                              <button
+                                onClick={() =>
+                                  toggleCardExpansion(
+                                    expandedGrantId === claim.id
+                                      ? null
+                                      : claim.id
+                                  )
+                                }
+                                className="text-green-700 text-md hover:text-green-900 transition duration-150 ease-in-out"
+                              >
+                                {expandedGrantId === claim.id
+                                  ? "Less Details"
+                                  : "View Details"}
+                              </button>
+                            </div>
+
+                            <div className="border-t border-gray-300 mb-5"></div>
+                            {expandedGrantId === claim.id ? (
+                              <div>
+                                {/* Expanded view content */}
+                                <div className="mb-5">
+                                  <div className="flex justify-between">
+                                    <h2 className="text-black font-semibold mb-2">
+                                      Grant's Information
+                                    </h2>
+                                    <h2 className="text-black mb-2">
+                                      Learn more (a link)
+                                    </h2>
+                                  </div>
+                                  <div className="bg-slate-100 border-2 rounded p-6">
+                                    <div className="flex justify-between mb-2">
+                                      <span className="font-bold">
+                                        Category:
+                                      </span>
+                                      {claim.category}
+                                    </div>
+                                    <div className="flex justify-between mb-2">
+                                      <span className="font-bold">
+                                        Provider:
+                                      </span>
+                                      Rawad
+                                    </div>
+                                    <div className="flex justify-between items-center mb-2">
+                                      <span className="font-bold">
+                                        Amount Payable:
+                                      </span>
+                                      {claim.amountPayable}
+                                    </div>
+                                    <div className="mb-2">
+                                      <span class="font-bold">
+                                        Description:
+                                      </span>
+                                    </div>
+                                    <div className="mb-2">
+                                      This is a really great grant! This is a
+                                      really great grant! This is a really great
+                                      grant! This is a really great grant! This
+                                      is a really great grant! This is a really
+                                      great grant! This is a really great grant!
+                                      This is a really great grant! This is a
+                                      really great grant! This is a really great
+                                      grant!
+                                    </div>
+                                    <div className="flex justify-between mb-2">
+                                      <span className="font-bold">
+                                        Date Posted:
+                                      </span>
+                                      2022-12-20
+                                    </div>
+                                    {/* add more  */}
+                                  </div>
+                                </div>
+                                <h2 className="text-black font-semibold mb-2">
+                                  Applicant's Information
+                                </h2>
+                                <div className="bg-slate-100 border-2  rounded p-6">
+                                  <div className="flex justify-between mb-4 text-md">
+                                    <span className="font-bold">
+                                      Applicant Name:
+                                    </span>
+                                    {claim.applicantName}
+                                  </div>
+                                  <div className="flex justify-between mb-4 text-md">
+                                    <span className="font-bold">Age:</span>
+                                    18
+                                  </div>
+                                  <div className="flex justify-between mb-4 text-md">
+                                    <span className="font-bold">
+                                      Nationality:
+                                    </span>
+                                    Canadian
+                                  </div>
+                                  <div className="flex justify-between items-center mb-2">
+                                    <span className="font-bold">
+                                      Date Submitted:
+                                    </span>
+                                    {claim.dateSubmitted}
+                                  </div>
+                                  {/* add more */}
+                                </div>
+                                <h2 className="text-black font-semibold mb-2 mt-5">
+                                  Applicant's Response
+                                </h2>
+                                <div className="bg-slate-100 border-2  rounded p-6">
+                                  <div className="mb-2">
+                                    <span class="font-bold">
+                                      Question 1:{" "}
+                                      <span className="text-red-400">
+                                        Why do you need this grant?
+                                      </span>
+                                    </span>
+                                  </div>
+                                  <div className="mb-8 mt-2">
+                                    <span class="font-bold">
+                                      Answer:{" "}
+                                      <span className="text-red-400">
+                                        I am broke af
+                                      </span>
+                                    </span>
+                                  </div>
+                                  <div className="mb-2">
+                                    <span class="font-bold">
+                                      Question 2:{" "}
+                                      <span className="text-red-400">
+                                        Why do you need this grant?
+                                      </span>
+                                    </span>
+                                  </div>
+                                  <div className="mb-8 mt-2">
+                                    <span class="font-bold">
+                                      Answer:{" "}
+                                      <span className="text-red-400">
+                                        I am broke af
+                                      </span>
+                                    </span>
+                                  </div>
+                                  <div className="mb-2">
+                                    <span class="font-bold">
+                                      Question 3:{" "}
+                                      <span className="text-red-400">
+                                        Why do you need this grant?
+                                      </span>
+                                    </span>
+                                  </div>
+                                  <div className="mb-8 mt-2">
+                                    <span class="font-bold">
+                                      Answer:{" "}
+                                      <span className="text-red-400">
+                                        I am broke af
+                                      </span>
+                                    </span>
+                                  </div>
+                                  {/* add more */}
+                                </div>
+                              </div>
+                            ) : (
+                              <div>
+                                {/* Small view content */}
+                                <p className="mb-2 text-md">
+                                  {claim.applicantName}
+                                </p>
+                                <p className="mb-2">{claim.category}</p>
+                                <div className="flex justify-between items-center">
+                                  <div>
+                                    <p>Date Submitted:</p>
+                                    <p>{claim.dateSubmitted}</p>
+                                  </div>
+                                  <div>
+                                    <p>Amount Payable:</p>
+                                    <p>{claim.amountPayable}</p>
+                                  </div>
+                                </div>
+                              </div>
+                            )}
+                          </div>
                         </div>
-                        <div className="border-t border-gray-300 mb-5"></div>
-
-                        <p className="mb-2 text-md">{claim.applicantName}</p>
-
-                        <p className="mb-2">{claim.category}</p>
-
-                        <div className="flex justify-between items-center">
-                          <p>Date Submitted: {claim.dateSubmitted}</p>
-                          <p>Amount Payable: ${claim.amountPayable}</p>
-                        </div>
-                      </div>
-                    </div>
-                  ))}
+                      );
+                    }
+                    return null; // If the card should not be displayed, return null to skip rendering
+                  })}
                 </div>
               ) : (
                 <div className="text-center mt-20 mb-10 text-black">
