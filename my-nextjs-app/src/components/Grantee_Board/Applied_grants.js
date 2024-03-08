67391e1622cec5f9e47e549f19d7cd717baec0ba
Austin <austinjas014@gmail.com>
2024-03-08 17:32:37 -0500

    Added view details

--- my-nextjs-app/src/components/Grantee_Board/Applied_grants.js
+++ my-nextjs-app/src/components/Grantee_Board/Applied_grants.js
@@ -37,7 +37,7 @@ const Applied_Grants = () => {
       status: "Approved",
     },
     {
-      id: 1,
+      id: 4,
       serviceDate: "2023-01-01",
       receivedOn: "2023-01-02",
       claimType: "Type A",
@@ -48,7 +48,7 @@ const Applied_Grants = () => {
       status: "Approved",
     },
     {
-      id: 2,
+      id: 5,
       serviceDate: "2023-02-01",
       receivedOn: "2023-02-05",
       claimType: "Type B",
@@ -59,7 +59,7 @@ const Applied_Grants = () => {
       status: "In Review",
     },
     {
-      id: 3,
+      id: 6,
       serviceDate: "2023-03-01",
       receivedOn: "2023-03-03",
       claimType: "Type C",
@@ -70,7 +70,7 @@ const Applied_Grants = () => {
       status: "Approved",
     },
     {
-      id: 1,
+      id: 7,
       serviceDate: "2023-01-01",
       receivedOn: "2023-01-02",
       claimType: "Type A",
@@ -81,7 +81,7 @@ const Applied_Grants = () => {
       status: "Submitted",
     },
     {
-      id: 2,
+      id: 8,
       serviceDate: "2023-02-01",
       receivedOn: "2023-02-05",
       claimType: "Type B",
@@ -92,7 +92,7 @@ const Applied_Grants = () => {
       status: "In Review",
     },
     {
-      id: 3,
+      id: 9,
       serviceDate: "2023-03-01",
       receivedOn: "2023-03-03",
       claimType: "Type C",
@@ -103,7 +103,7 @@ const Applied_Grants = () => {
       status: "Approved",
     },
     {
-      id: 2,
+      id: 10,
       serviceDate: "2023-02-01",
       receivedOn: "2023-02-05",
       claimType: "Type B",
@@ -114,7 +114,7 @@ const Applied_Grants = () => {
       status: "In Review",
     },
     {
-      id: 3,
+      id: 11,
       serviceDate: "2023-03-01",
       receivedOn: "2023-03-03",
       claimType: "Type C",
@@ -125,7 +125,7 @@ const Applied_Grants = () => {
       status: "Approved",
     },
     {
-      id: 2,
+      id: 12,
       serviceDate: "2023-02-01",
       receivedOn: "2023-02-05",
       claimType: "Type B",
@@ -136,7 +136,7 @@ const Applied_Grants = () => {
       status: "In Review",
     },
     {
-      id: 3,
+      id: 13,
       serviceDate: "2023-03-01",
       receivedOn: "2023-03-03",
       claimType: "Type C",
@@ -147,7 +147,7 @@ const Applied_Grants = () => {
       status: "Approved",
     },
     {
-      id: 2,
+      id: 14,
       serviceDate: "2023-02-01",
       receivedOn: "2023-02-05",
       claimType: "Type B",
@@ -158,7 +158,7 @@ const Applied_Grants = () => {
       status: "In Review",
     },
     {
-      id: 3,
+      id: 15,
       serviceDate: "2023-03-01",
       receivedOn: "2023-03-03",
       claimType: "Type C",
@@ -172,7 +172,7 @@ const Applied_Grants = () => {
 
   const grantsPerPage = 6;
   const [currentPage, setCurrentPage] = useState(1);
-  const [bgWhite, setBgWhite] = useState(false); // State to toggle background color
+  const [expandedGrantId, setExpandedGrantId] = useState(null);
 
   const indexOfLastGrant = currentPage * grantsPerPage;
   const indexOfFirstGrant = indexOfLastGrant - grantsPerPage;
@@ -180,6 +180,16 @@ const Applied_Grants = () => {
 
   const paginate = (pageNumber) => {
     setCurrentPage(pageNumber);
+    setExpandedGrantId(null); // Collapse any expanded card when paginating
+  };
+
+  const toggleCardExpansion = (id) => {
+    if (expandedGrantId === id) {
+      // If already expanded, shrink the card
+      setExpandedGrantId(null);
+    } else {
+      setExpandedGrantId(id);
+    }
   };
 
   const getStatusColor = (status) => {
@@ -207,14 +217,19 @@ const Applied_Grants = () => {
     >
       <div className="flex justify-center w-full">
         <div className="max-w-7xl w-full mx-auto px-4">
-          <div className="mt-10 rounded-lg shadow overflow-hidden">
+          <div className="mt-10 bg-sky-50 rounded-lg shadow overflow-hidden">
             <h1 className="text-black lg:text-4xl md:text-3xl text-3xl text-center sm:text-left font-semibold mb-6 p-6">
               Rawad's Application History
             </h1>
 
             <div className="flex lg:flex-row md:flex-row flex-col">
               <div className="lg:w-1/4 md:w-1/4 w-full p-4">
-                <h2 className="font-bold text-lg mb-4 text-black">Filters</h2>
+                <div className="flex justify-between items-center">
+                  <h2 className="font-bold text-lg mb-4 text-black">Filters</h2>
+                  <button className=" mb-4 text-sm font-semibold bg-gray-500 text-white px-3 py-1 rounded hover:bg-green-700 transition ease-in-out duration-150">
+                    Reset Filter
+                  </button>
+                </div>
                 {/* Filter form */}
                 <form>
                   <div className="mb-4">
@@ -303,40 +318,216 @@ const Applied_Grants = () => {
                   Applied Grants
                 </h2>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 text-black mt-3">
-                  {currentGrants.map((claim) => (
-                    <div key={claim.id} className="p-1">
-                      <div
-                        className={`${getStatusColor(
-                          claim.status
-                        )} h-2 w-full rounded-t-lg`}
-                      ></div>
+                  {currentGrants.map((claim) => {
+                    // Check if the card should be expanded or if no card is expanded
+                    const shouldDisplay =
+                      expandedGrantId === null || expandedGrantId === claim.id;
 
-                      {/* Card content */}
-                      <div className="bg-white p-2 rounded-lg shadow shadow-xl">
-                        <div className="flex justify-between items-center mb-4">
-                          <p className="text-md">Status: {claim.status}</p>
-                          <button className="text-green-700 text-md hover:text-green-900 transition duration-150 ease-in-out">
-                            View Details
-                          </button>
+                    // Only render the card if it should be displayed based on the above condition
+                    if (shouldDisplay) {
+                      return (
+                        <div
+                          key={claim.id}
+                          className={` p-1 transition-all duration-500 ease-in-out ${
+                            expandedGrantId === claim.id
+                              ? "scale-100 opacity-100"
+                              : "scale-95 opacity-75"
+                          } ${
+                            expandedGrantId === claim.id
+                              ? "col-span-3 lg:col-span-3"
+                              : "md:col-span-2 lg:col-span-1"
+                          }`}
+                        >
+                          <div
+                            className={`${getStatusColor(
+                              claim.status
+                            )} h-2 w-full rounded-t-lg`}
+                          ></div>
+
+                          <div className="bg-white p-2 rounded-lg shadow shadow-xl">
+                            <div className="flex justify-between items-center mb-4">
+                              <p className="text-md">Status: {claim.status}</p>
+                              <button
+                                onClick={() =>
+                                  toggleCardExpansion(
+                                    expandedGrantId === claim.id
+                                      ? null
+                                      : claim.id
+                                  )
+                                }
+                                className="text-green-700 text-md hover:text-green-900 transition duration-150 ease-in-out"
+                              >
+                                {expandedGrantId === claim.id
+                                  ? "Less Details"
+                                  : "View Details"}
+                              </button>
+                            </div>
+
+                            <div className="border-t border-gray-300 mb-5"></div>
+                            {expandedGrantId === claim.id ? (
+                              <div>
+                                {/* Expanded view content */}
+                                <div className="mb-5">
+                                  <div className="flex justify-between">
+                                    <h2 className="text-black font-semibold mb-2">
+                                      Grant's Information
+                                    </h2>
+                                    <h2 className="text-black mb-2">
+                                      Learn more (a link)
+                                    </h2>
+                                  </div>
+                                  <div className="bg-slate-100 border-2 rounded p-6">
+                                    <div className="flex justify-between mb-2">
+                                      <span className="font-bold">
+                                        Category:
+                                      </span>
+                                      {claim.category}
+                                    </div>
+                                    <div className="flex justify-between mb-2">
+                                      <span className="font-bold">
+                                        Provider:
+                                      </span>
+                                      Rawad
+                                    </div>
+                                    <div className="flex justify-between items-center mb-2">
+                                      <span className="font-bold">
+                                        Amount Payable:
+                                      </span>
+                                      {claim.amountPaid}
+                                    </div>
+                                    <div className="mb-2">
+                                      <span class="font-bold">
+                                        Description:
+                                      </span>
+                                    </div>
+                                    <div className="mb-2">
+                                      This is a really great grant! This is a
+                                      really great grant! This is a really great
+                                      grant! This is a really great grant! This
+                                      is a really great grant! This is a really
+                                      great grant! This is a really great grant!
+                                      This is a really great grant! This is a
+                                      really great grant! This is a really great
+                                      grant!
+                                    </div>
+                                    <div className="flex justify-between mb-2">
+                                      <span className="font-bold">
+                                        Date Posted:
+                                      </span>
+                                      2022-12-20
+                                    </div>
+                                    {/* add more  */}
+                                  </div>
+                                </div>
+                                <h2 className="text-black font-semibold mb-2">
+                                  Applicant's Information
+                                </h2>
+                                <div className="bg-slate-100 border-2  rounded p-6">
+                                  <div className="flex justify-between mb-4 text-md">
+                                    <span className="font-bold">
+                                      Applicant Name:
+                                    </span>
+                                    {claim.memberName}
+                                  </div>
+                                  <div className="flex justify-between mb-4 text-md">
+                                    <span className="font-bold">Age:</span>
+                                    18
+                                  </div>
+                                  <div className="flex justify-between mb-4 text-md">
+                                    <span className="font-bold">
+                                      Nationality:
+                                    </span>
+                                    Canadian
+                                  </div>
+                                  <div className="flex justify-between items-center mb-2">
+                                    <span className="font-bold">
+                                      Date Submitted:
+                                    </span>
+                                    {claim.receivedOn}
+                                  </div>
+                                  {/* add more */}
+                                </div>
+                                <h2 className="text-black font-semibold mb-2 mt-5">
+                                  Applicant's Response
+                                </h2>
+                                <div className="bg-slate-100 border-2  rounded p-6">
+                                  <div className="mb-2">
+                                    <span class="font-bold">
+                                      Question 1:{" "}
+                                      <span className="text-red-400">
+                                        Why do you need this grant?
+                                      </span>
+                                    </span>
+                                  </div>
+                                  <div className="mb-8 mt-2">
+                                    <span class="font-bold">
+                                      Answer:{" "}
+                                      <span className="text-red-400">
+                                        I am broke af
+                                      </span>
+                                    </span>
+                                  </div>
+                                  <div className="mb-2">
+                                    <span class="font-bold">
+                                      Question 2:{" "}
+                                      <span className="text-red-400">
+                                        Why do you need this grant?
+                                      </span>
+                                    </span>
+                                  </div>
+                                  <div className="mb-8 mt-2">
+                                    <span class="font-bold">
+                                      Answer:{" "}
+                                      <span className="text-red-400">
+                                        I am broke af
+                                      </span>
+                                    </span>
+                                  </div>
+                                  <div className="mb-2">
+                                    <span class="font-bold">
+                                      Question 3:{" "}
+                                      <span className="text-red-400">
+                                        Why do you need this grant?
+                                      </span>
+                                    </span>
+                                  </div>
+                                  <div className="mb-8 mt-2">
+                                    <span class="font-bold">
+                                      Answer:{" "}
+                                      <span className="text-red-400">
+                                        I am broke af
+                                      </span>
+                                    </span>
+                                  </div>
+                                  {/* add more */}
+                                </div>
+                              </div>
+                            ) : (
+                              <div>
+                                {/* Small view content */}
+                                <p className="mb-2 text-md">
+                                  {claim.memberName}
+                                </p>
+                                <p className="mb-2">{claim.category}</p>
+                                <div className="flex justify-between items-center">
+                                  <p>Date Submitted: {claim.receivedOn}</p>
+                                  <p>Amount Payable: {claim.amountPaid}</p>
+                                </div>
+                              </div>
+                            )}
+                          </div>
                         </div>
-                        <div className="border-t border-gray-300 mb-5"></div>
-                        <p className="mb-2 text-md">{claim.memberName}</p>
-
-                        <p className="mb-2">{claim.category}</p>
-
-                        <div className="flex justify-between items-center">
-                          <p>Date Submitted: {claim.receivedOn}</p>
-                          <p>Amount Payable: {claim.amountPaid}</p>
-                        </div>
-                      </div>
-                    </div>
-                  ))}
+                      );
+                    }
+                    return null; // If the card should not be displayed, return null to skip rendering
+                  })}
                 </div>
+
                 <div className="flex justify-center p-4 text-black">
                   {currentPage > 1 && (
                     <button
                       onClick={() => paginate(currentPage - 1)}
-                      className="px-4 py-2 mr-5 text-sm font-semibold text-white bg-green-600 rounded-full w-1/4 hover:bg-green-800"
+                      className="px-2 py-2 mr-5 text-sm font-semibold text-white bg-green-600 rounded-full w-1/4 hover:bg-green-800"
                     >
                       Previous
                     </button>
