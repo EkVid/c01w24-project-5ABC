718e86ea19df5c5418cdef9a273b606429a8928e
Austin <austinjas014@gmail.com>
2024-03-17 14:57:54 -0400

    Changed the tabs

--- my-nextjs-app/src/components/navbar/AuthNavBar.js
+++ my-nextjs-app/src/components/navbar/AuthNavBar.js
@@ -63,7 +63,7 @@ const Navbar = () => {
                 href="#"
                 className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
               >
-                About
+                Search Grants
               </a>
             </li>
             <li>
@@ -71,15 +71,7 @@ const Navbar = () => {
                 href="#"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
-                Services
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Pricing
+                Applied Grants
               </a>
             </li>
             <li>
