5a12160a3c6a38138344c8a847ca28f53912fa34
alykhan02 <aversi07@gmail.com>
2024-03-18 14:36:50 -0400

    sample commit

--- my-nextjs-app/src/app/(root)/page.js
+++ my-nextjs-app/src/app/(root)/page.js
@@ -3,20 +3,17 @@ import Hero from "@/components/Hero";
 import Quotes from "@/components/Quotes/Quotes";
 // import AuthNavBar from "@/components/navbar/AuthNavBar";
 import UnauthNavBar from "@/components/navbar/UnauthNavBar";
-<<<<<<< HEAD:my-nextjs-app/src/app/(root)/page.js
-<<<<<<< HEAD:my-nextjs-app/src/app/(root)/page.js
-=======
-import Login from "@/components/Login";
-import SignUp from "@/components/Signup";
 
->>>>>>> c0cda00 (Created sign up and sign in sections' UI):my-nextjs-app/src/app/page.js
-=======
->>>>>>> 548595b (Added routes for login & Signup):my-nextjs-app/src/app/page.js
+
+import Login from "@/components/signup-login/Login";
+import SignUp from "@/components/signup-login/Signup";
+
+
 export default function Home() {
 
   return (
     <main>
-      <AuthNavBar />
+      <UnauthNavBar />
       <AddFormButton />
       <Hero />
       <Quotes />
--- my-nextjs-app/src/app/layout.js
+++ my-nextjs-app/src/app/layout.js
@@ -1,13 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import dynamic from "next/dynamic";
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 import Footer from "@/components/Footer";
->>>>>>> c0cda00 (Created sign up and sign in sections' UI)
-=======
->>>>>>> 972d39f (moved routers and removed warnings in inspect console)
+
 
 const AccessibilityBar = dynamic(
   () => import("@/components/AccessibilityBar"),
@@ -23,25 +18,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html id="root" lang="en" suppressHydrationWarning={true}>
-      <body className={inter.className} >
-        <main className="flex flex-col flex-grow custom-offwhite-background dark:bg-[#1f1f1f]">
-          {children}
-        </main>
-<<<<<<< HEAD
     <html id="root" lang="en">
       <body className={inter.className}>
         <div className="flex flex-col min-h-screen">
           <AccessibilityBar />
           <main className="flex-grow dark:bg-[#1f1f1f]">{children}</main>
-<<<<<<< HEAD
           <Footer />
-=======
-          {showFooter && <Footer />}
->>>>>>> 548595b (Added routes for login & Signup)
         </div>
-=======
->>>>>>> 0996ef1 (grants integrations)
       </body>
     </html>
   );
