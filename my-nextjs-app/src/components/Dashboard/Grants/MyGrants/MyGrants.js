3b2bad0729e6974f89b6e3d211fda0747b9544a1
CarterMorris <carter.morris@mail.utoronto.ca>
2024-03-24 21:03:29 -0400

    Added accessibility for my grants page

--- my-nextjs-app/src/components/Dashboard/Grants/MyGrants/MyGrants.js
+++ my-nextjs-app/src/components/Dashboard/Grants/MyGrants/MyGrants.js
@@ -20,22 +20,40 @@ const MyGrants = ({ grants }) => {
             const grantStatus = getGrantStatus(grant)
             
             return( 
-                <details className="group px-4 py-4 my-4 rounded-md border border-black dark:border-white" key={grant._id}>
+                <details aria-label={`${grant.Title} dropdown`} className="group px-4 py-4 my-4 rounded-md border border-black dark:border-white" key={grant._id}>
                     <summary className="flex flex-col sm:flex-row hover:cursor-pointer group-open:mb-5 justify-between items-center">
                         <h2 className="dark:d-text text-xl text-centerfont-bold">{grant.Title}</h2>
                         {/* Set proper color classes after merch with applicant dashboard */}
-                        <div className={`rounded-full text-center text-white px-4 py-2 mt-2 sm:mt-0 ${grantStatus === 'Awarded' ? 'custom-green-background' : (grantStatus === 'Open' ? 'bg-[#d1aa64]' : 'bg-[#d76b65]')}`}>
+                        <div tabIndex="0" aria-label={`Grant status: ${grantStatus}`} className={`rounded-full text-center text-white px-4 py-2 mt-2 sm:mt-0 ${grantStatus === 'Awarded' ? 'custom-green-background' : (grantStatus === 'Open' ? 'bg-[#d1aa64]' : 'bg-[#d76b65]')}`}>
                             {grantStatus}
                         </div>
                     </summary>
 
-                    <div className="flex flex-col gap-y-2 sm:gap-y-0">
-                        <p className="pt-2 dark:d-text">Description: {grant.Description}</p>
-                        <p className="pt-2 dark:d-text">Grant Winners Picked: {grant.NumWinners}</p>
-                        <p className="pt-2 dark:d-text">Maximum Number of Grant Winners: {grant.MaxWinners}</p>
-                        <p className="pt-2 dark:d-text">Amount Per Winner (CAD $): {grant.AmountPerApp}</p>
-                        <p className="pt-2 dark:d-text">Application Deadline: {grant.Deadline}</p>
-
+                    <div className="flex flex-col gap-y-2 dark:d-text sm:gap-y-0">
+                        <div className="flex flex-col sm:flex-row justify-between">
+                            <p tabIndex="0" className="pt-2 pe-2 font-bold">Description:</p> 
+                            <p tabIndex="0">{grant.Description}</p>
+                        
+                        </div>
+                        <div className="flex flex-col sm:flex-row justify-between">
+                            <p tabIndex="0" className="pt-2 pe-2 font-bold">Grant Winners Picked:</p> 
+                            <p tabIndex="0">{grant.NumWinners}</p>
+                        
+                        </div>
+                        <div className="flex flex-col sm:flex-row justify-between">
+                            <p tabIndex="0" className="pt-2 pe-2 font-bold">Maximum Number of Grant Winners:</p> 
+                            <p tabIndex="0">{grant.MaxWinners}</p>
+                        
+                        </div>
+                        <div className="flex flex-col sm:flex-row justify-between">
+                            <p tabIndex="0" className="pt-2 pe-2 font-bold">Amount Per Winner (CAD $):</p> 
+                            <p tabIndex="0">{grant.AmountPerApp}</p>
+                        </div>
+                        <div className="flex flex-col sm:flex-row justify-between">
+                            <p tabIndex="0" className="pt-2 pe-2 font-bold">Application Deadline:</p> 
+                            <p tabIndex="0">{grant.Deadline}</p>
+                        </div>
+                        
                         <div className="w-full flex flex-col sm:flex-row">
                             {grant.Active ? 
                                 <button 
@@ -44,6 +62,7 @@ const MyGrants = ({ grants }) => {
                                         router.refresh()
                                     }}
                                     className="rounded text-center px-4 py-2 hover:scale-105 text-white mt-6 bg-[#d76b65]"
+                                    aria-label="Change grant status to closed"
                                 >
                                     Close Grant
                                 </button>
@@ -55,6 +74,7 @@ const MyGrants = ({ grants }) => {
                                             router.refresh()
                                         }}
                                         className="rounded text-center px-4 py-2 hover:scale-105 text-white mt-6 custom-green-background"
+                                        aria-label="Change grant status to open"
                                     >
                                         Open Grant
                                     </button>
