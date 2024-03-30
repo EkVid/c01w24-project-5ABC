e7e1cd4a81011d8c6f7195f1805c5371942330fa
BaNg-W <bang344251871@gmail.com>
2024-03-29 20:35:48 -0400

    Missing files

---
+++ backend/helpers.py
@@ -0,0 +1,31 @@
+import json as JSON
+
+"""Returns the JSON data from the given multipart form data request.
+"""
+def getJSONData(request):
+    contentType = request.headers.get("Content-Type", None)
+    if contentType is None or request.headers.get("Content-Type").split(";")[0] != "multipart/form-data":
+        return None
+
+    return JSON.loads(request.form.get("jsonData", "null"))
+
+
+def getFileData(request):
+    contentType = request.headers.get("Content-Type", None)
+    if contentType is None or request.headers.get("Content-Type").split(";")[0] != "multipart/form-data":
+        return None
+
+    return JSON.loads(request.form.get("files", "null"))
+
+
+"""Returns True if `listCandidate` is of type list[dict] and False otherwise.
+"""
+def isListOfDict(listCandidate):
+    if not isinstance(listCandidate, list):
+        return False
+
+    for dictCandidate in listCandidate:
+        if not isinstance(dictCandidate, dict):
+            return False
+
+    return True
